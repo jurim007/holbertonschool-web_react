956da9e6f575c3c7e2fc6844080b9463e23c034e
007codename <juri.malaj@gmail.com>
2026-09-25 17:48:36 +0200

    task 2: fixed and ready for test correction

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -1,26 +1,28 @@
-import logo from "./assets/holberton-logo.jpg";
+import './App.css'
+import holbertLogo from "./assets/holberton-logo.jpg";
+import Notifications from './Notifications.jsx';
+import { getCurrentYear, getFooterCopy } from './utils';
 
-import "./App.css";
-
-const App = () => {
-  const date = new Date();
+function App() {
 
   return (
     <>
-      <div className="App-header">
-        <img src={logo} alt="holberton logo" />
+      <div>
+        <Notifications />
+      </div>
+      <div className='App-header'>
+        <img src={holbertLogo} alt='holberton logo' />
         <h1 style={{ color: "#e1003c" }}>School dashboard</h1>
       </div>
 
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
+      <div className='App-body'>
+        <p> Login to access the full dashboard</p>
       </div>
-
       <div className='App-footer'>
         <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
