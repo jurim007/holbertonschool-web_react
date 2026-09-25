6edcb4d7ab3a9664d4798631fd43325263fa3916
007codename <juri.malaj@gmail.com>
2026-09-25 18:20:52 +0200

    task 4: finished and ready for test correction

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -1,25 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders h1 with School Dashboard text", () => {
-  render(<App />);
-  const heading = screen.getByRole("heading", { level: 1, name: /school dashboard/i });
-  expect(heading).toBeInTheDocument();
-});
-
-test("renders correct text in body and footer paragraphs", () => {
+test("renders App form correctly", () => {
   render(<App />);
 
-  const bodyText = screen.getByText(/login to access the full dashboard/i);
-  expect(bodyText).toBeInTheDocument();
+  const emailInput = screen.getByLabelText(/email/i);
+  const passwordInput = screen.getByLabelText(/password/i);
+  expect(emailInput).toBeInTheDocument();
+  expect(passwordInput).toBeInTheDocument();
 
-  const footerRegex = /copyright \d{4}.*holberton school/i;
-  const footerNode = screen.getByText(footerRegex);
-  expect(footerNode).toBeInTheDocument();
-});
+  const emailLabel = screen.getByText(/email/i);
+  const passwordLabel = screen.getByText(/password/i);
+  expect(emailLabel).toBeInTheDocument();
+  expect(passwordLabel).toBeInTheDocument();
 
-test("renders an image", () => {
-  render(<App />);
-  const image = screen.getByAltText(/holberton logo/i);
-  expect(image).toBeInTheDocument();
+  const button = screen.getByRole("button", { name: /^ok$/i });
+  expect(button).toBeInTheDocument();
 });
